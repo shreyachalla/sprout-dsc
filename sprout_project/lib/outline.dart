cdd2469a1ada40c2d97e57aefe3051d17593a9e0
rluk3 <rluk3@gatech.edu>
2021-03-18 20:22:37 -0700

    include home element in outline

--- sprout_project/lib/outline.dart
+++ sprout_project/lib/outline.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'news.dart';
 import 'portfolio.dart';
 import 'home.dart';
-
 import 'future.dart';
+import 'login.dart';
 
 //import 'stocks.dart';
 
@@ -11,12 +11,65 @@ void main() {
   runApp(Outline());
 }
 
-class OutlineState extends State<Outline> {
+class Outline extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Sprout'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Looking good, [person].',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            RaisedButton(
+              textColor: Colors.blue,
+              color: Colors.white,
+              child: Text('Here is how'),
+              onPressed: () {
+                navigateToSubPage(context);
+              },
+            ),
+            Text(
+              'you can do better.',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(5, 50, 300, 2),
+              alignment: Alignment.bottomLeft,
+              child: IconButton(
+                  icon: Icon(
+                    Icons.attach_money_outlined,
+                  ),
+                  onPressed: () {
+                    return showDialog(
+                      context: context,
+                      builder: (ctx) => AlertDialog(
+                        title: Text("Tip of the Day"),
+                        content: Text(
+                            "Investing in healthcare ASAP could give you a booming return, recent studies show."),
+                        actions: <Widget>[
+                          FlatButton(
+                            onPressed: () {
+                              Navigator.of(ctx).pop();
+                            },
+                            child: Text("Great!"),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
@@ -54,9 +107,7 @@ class OutlineState extends State<Outline> {
               label: 'News'),
           BottomNavigationBarItem(
               icon: IconButton(
-                icon: Icon(
-                  Icons.laptop_chromebook_sharp,
-                ),
+                icon: Icon(Icons.access_time),
                 onPressed: () {
                   navigateToFuturePage(context);
                 },
@@ -65,6 +116,10 @@ class OutlineState extends State<Outline> {
         ],
       ),
     );
+  }
+
+  Future navigateToSubPage(context) async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => SubPage()));
   }
 
   Future navigateToPortfolioPage(context) async {
@@ -78,18 +133,16 @@ class OutlineState extends State<Outline> {
   }
 
   Future navigateToHomePage(context) async {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => MainPage()));
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Outline()));
   }
 
   Future navigateToFuturePage(context) async {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => FuturePage()));
+    Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()));
   }
 }
 
-class Outline extends StatefulWidget {
+/*class Outline extends StatefulWidget {
   State<StatefulWidget> createState() {
     return OutlineState();
   }
-}
+}*/
